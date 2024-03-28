563bdddc92a1bb8c085b3566cc4cf0fc4a04325f
UrName51 <98849399+UrName51@users.noreply.github.com>
2024-03-28 15:21:34 +0000

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "ACS Egham ROAR",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "",
+    bio: "The Official ACS Egham Newsletter",
+    email: "",
+    linkedin: "",
+    github: "",
     instagram: "",
   },
   projects: [
