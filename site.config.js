9e58dff11372fed7a3d54e0c7d9403b6be0f26e5
UrName51 <98849399+UrName51@users.noreply.github.com>
2024-05-22 12:36:58 +0100

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,14 +18,14 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "ACS ROAR",
+    description: "welcome to the ACS ROAR website!",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "https://acs-roar-q2kw.vercel.app",
+  since: 2024, // If leave this empty, current year will be used.
+  lang: "en-UK", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
